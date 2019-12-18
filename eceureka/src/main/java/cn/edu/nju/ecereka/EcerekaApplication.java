00ecefa133aa98aa3a08e0c46c348067420abc5d
zhenyuxie <zhenyu.xie@transwarp.io>
2019-12-18 20:04:24 +0800

    Initial commit

---
+++ eceureka/src/main/java/cn/edu/nju/ecereka/EcerekaApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.nju.ecereka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcerekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcerekaApplication.class, args);
+    }
+
+}
---
+++ eczuul/src/main/java/cn/edu/nju/eczuul/EczuulApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.nju.eczuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EczuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EczuulApplication.class, args);
+    }
+
+}
